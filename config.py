87df90e6e287262fadbfb018d0578b0e5928c715
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2023-05-31 20:07:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 import logging
 class Config:                                                                   
-    API_ID = int(os.environ.get("API_ID", "24720215"))
-    API_HASH = os.environ.get("API_HASH", "c0d3395590fecba19985f95d6300785e")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5858006797:AAHvD6U81nTd40oK5r_PSq0Q0HUWy__GVoI")
+    API_ID = int(os.environ.get("API_ID", "19911978"))
+    API_HASH = os.environ.get("API_HASH", "e3f5848d4c384af9e6f1f52ca84c19c7")       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5881103857:AAHalNAze8aCfNsg28TOyrq_Cdc82NIoOHw")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
-    OWNER_ID = os.environ.get("OWNER_ID", "6295565062")                             
+    OWNER_ID = os.environ.get("OWNER_ID", "6205468574")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Nischaybot:Nischaybot@cluster0.thf9kzz.mongodb.net/?retryWrites=true&w=majority")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
-    SESSION = os.environ.get("SESSION", "BQF5M1cAoCtVPAPW-yFetlKbxbjuEjqKkOGDv-i0-BGtXkVmbGr_0Qa26Em_aZow-AYxungNmXVKc8a1cbKbzqxisYGcrDoDyHj2P-SuPG-qsfaekxJPDPoLAEhLX-CfXcHCs3HTiOkNG9JsVSC2mmvYxLjOQxNn6UfZr4Y_tQSfAW1Ex7_UBvytbuUTVnhkk-ek_4tQ2HdcSzpOTFHD1W3DzjmkP92Py9bpNG5XzAUAvZ6R-yHxp_jdXGM7dhd-LvPGnrVR87ufVX1JJma_pvLOz4b6Cra0paCGTC_L55g7dnV8wGJeL-GN3y1DZ6Uoy6r67Qw4PlCWxnrzao7d1Jzz82b4EAAAAAF3PrMGAA")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001553031624"))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", "NY_Forward_robot")
+    SESSION = os.environ.get("SESSION", "BQEv1SoAYi5uLHrFQ9u_7ncA78tbcWP-7KCV7O2tPMg4w6aPSqAGYjZwBS7chvsKVdfvDjOfurGoIsEjDCNSavt0QkSl9MucB9jKDoxMrFz_M_evNISH-ZMGX7f6I48s_m7DfRk3YJ_OmZ717YjdLiHWY63wkBsDysFuN9zWaARrbQRgzZ-CHsVsPQSjmFNzUQ_t83Fl5TqwprU8_1lLiOayHmgCoP3b8svcYTXd9avsFL6_TrznZct4My11lYkDiOoo0ChV9yYyy9bnFH4OH4NNqw-OSpqIhXHK0QgjUHzdPGILtx3INdk0qcxN4tlOPMhuqDEadyILggFwAMS-W5jQ8pA1ZQAAAAFx3eAA")   
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001811894401"))
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "Forward_Ny_bot")
 
 
 def LOGGER(name: str) -> logging.Logger:
